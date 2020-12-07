767c99fca2b3833c13a9355c424a079c3b84ad29
Raph <rsuttiyotin@seek.com.au>
2020-12-07 14:06:23 +1100

    Add giphy search

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
   const [search, setSearch] = useState('')
-
-  function onSubmit(e) {
-    e.preventDefault();
-    console.log(search)
-  }
+  const [query, setQuery] = useState('')
+  const [results, setResults] = useState([])
+  
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = fetch(`https://api.giphy.com/v1/gifs/search?api_key=YidoSBBg4BMFmJkxVqBSWi8WikKXICPm&q=${query}&limit=10&offset=0&rating=g&lang=en`)
+        const json = await (await response).json()
+        console.log({json})
+        setResults(json.data.map(item=>{
+          return item.images.preview.mp4
+        }))
+      } catch (error) {}
+    }
+    if(query !== '') {
+      fetchData()
+    }
+  },[query])
   
   return (
     <div className="App">
       <h1>GIFs search</h1>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={e => {
+        e.preventDefault();
+        setQuery(search)
+      }}>
         <input
         value={search}
         onChange={e => setSearch(e.target.value)}
@@ -20,6 +36,10 @@ function App() {
           />
         <button type="submit">Search</button>
       </form>
+      <br/>
+      {results.map(item => 
+          (<video autoPlay loop key={item} src={item}/>))
+      }
     </div>
   );
 }
